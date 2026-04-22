a3d5d022b643bde647f6041dc6546e48d15b3f19
Sam Steven <samfnatixsteven4@gmail.com>
2026-04-23 02:09:18 +0530

    added access,ref tokens

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,13 +1,15 @@
-import mongoose, {Schema} from 'mongoose'
-
+import mongoose, { Schema } from 'mongoose'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import crypto from 'crypto'
 
 const userSchema = new Schema({
-    avatar:{
-        type:{
+    avatar: {
+        type: {
             url: String,
             localPath: String
         },
-        default:{
+        default: {
             url: 'https://shorturl.at/UHHzl',
             localPath: ""
         }
@@ -55,8 +57,39 @@ const userSchema = new Schema({
         type: Date
     }
 
-},{
+}, {
     timestamps: true
 })
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username
+    }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: process.env.ACCESS_TOKEN_EXPIRY })
+}
+
+userSchema.methods.generateRefreshToken = function (){
+    return jwt.sign({
+        _id: this._id,
+    }, process.env.REFRESH_TOKEN_SECRET, { expiresIn: process.env.REFRESH_TOKEN_EXPIRY })
+}
+
+userSchema.methods.generateTempToken = function(){
+    const unHashedToken =  crypto.randomBytes(20).toString("hex")
+    const hashedToken = crypto.createHash("sha256").update(unHashedToken).digest("hex")
+    const tokenExpiry = Date.now()+(20*60*1000) //20mins
+    return {unHashedToken,hashedToken,tokenExpiry}
+}
 
 export const User = mongoose.model("User", userSchema)
